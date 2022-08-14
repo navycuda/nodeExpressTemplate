b3c56eb08b4ca63e43b9bcfecb159a6f5cce844b
Shane <navysupra@gmail.com>
2022-08-14 20:47:53 +0000

    fleshing out more functions and basic features

--- server.js
+++ server.js
@@ -1,10 +1,30 @@
 /* Require */
+const ncc = require('@navycuda/lotide');
+const morgan = require('morgan');
+const express = require('express');
+const app = express();
+const bcrypt = require('bcryptjs');
+const methodOverride = require('method-override');
+const cookieSession = require('cookie-session');
+const tools = require('./tools');
+
 /* Tcp:Http */
+const tcpHttp = {
+  Port: 8080,
+  cookieSession: {
+    name: "enigmaSecure",
+    keys: [ 'EKMFLGDQVZNTOWYHXUSPAIBRCJ', 'BDFHJLCPRTXVZNYEIWGAKMUSQO' ]
+  }
+};
+
 /* Middleware */
-/* Arguments & Properties */
-/* Local Functions */
-/* Export Functions */
-/* Classes */
+app.use(morgan('dev'));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static('public'));
+app.use(cookieSession(tcpHttp.cookieSession));
+app.use(methodOverride('_method'));
+app.set('view engine', 'ejs');
+
 /* Endpoints */
 ///* Create */
 ///* Read */
@@ -15,5 +35,15 @@
 ///* Edit */
 ///* Add */
 ///* Delete */
+
+/**
+ * ************** GET ************** *
+ * rx = request
+ * tx = response
+ */
+app.get('/', (rx, tx) => {
+
+});
+
 /* Execution & Test Data */
 /* Exports */
